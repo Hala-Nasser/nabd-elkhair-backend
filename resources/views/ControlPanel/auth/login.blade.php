ea66c1d9f8e561239161d9cce8c20c8f6ac658ff
Hala-Nasser <lola.el3sola2000@gmail.com>
2022-02-28 12:20:37 +0200

    test auth

---
+++ resources/views/ControlPanel/auth/login.blade.php
@@ -0,0 +1,85 @@
+@extends('ControlPanel.layouts.app')
+
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header form-group row">تسجيل الدخول</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">البريد الالكتروني</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert" dir="rtl">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">كلمة المرور</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                    
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                            @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}" style="color: #10B1B1; padding:0px;">
+                                        نسيت كلمة المرور؟
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary" style="background-color: #10B1B1; border-color: #10B1B1;">
+                                    تسجيل الدخول
+                                </button> 
+                            </div>
+                        </div>
+
+                        @if (Route::has('register'))
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                            <a class="nav-link" href="{{ route('register') }}" style="color: #10B1B1; padding: 0.5rem 0rem;">ليس لديك حساب؟</a>
+                            </div>
+                        </div>
+                        @endif
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
